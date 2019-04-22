c2e348878c90db515bffcbdae04e06ad32e41857
Artur Garcia <arturgarcia013@gmail.com>
2019-04-22 10:54:13 -0300

    Aula 2-4 2:3

---
+++ Assets/Scripts/MoveTwoPoints.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTwoPoints : MonoBehaviour
+{
+    public GameObject obj;
+    public Transform pos1, pos2;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
